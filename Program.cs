d31568067e7db52a46f484dc8e4d91966ec924fa
Vera <solnce288888@gmail.com>
2022-10-14 22:09:33 +0300

    num 36

--- Program.cs
+++ Program.cs
@@ -19,9 +19,29 @@ int CountEvens(int[] array){
 }
 
 
-/*Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+/*Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
 [3, 7, 23, 12] -> 19
 [-4, -6, 89, 6] -> 0*/
+
+Console.WriteLine("Введите размер массива: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Минимальное значение элемента массива: ");
+int minValue = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Максимальное значение элемента массива: ");
+int maxValue = int.Parse(Console.ReadLine()!);
+int[] array36 = GetArray(n, minValue, maxValue);
+Console.WriteLine($"Сгенерированный массив: [{String.Join(", ", array36)}]");
+Console.WriteLine($"Сумма элементов массива на нечетных позициях равна {SumOddPositions(array36)}");
+
+int SumOddPositions(int[] array){
+    int total = 0;
+    for(int i = 1; i < array.Length; i += 2){
+        total += array[i];
+    }
+    return total;
+}
+
 
 /*Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 [3 7 22 2 78] -> 76*/
